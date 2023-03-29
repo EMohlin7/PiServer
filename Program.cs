c6f722aa8dc8dfd3e569cd5faaf48b8aa53d812d
Edvin Mohlin <edvin.mohlin@gmail.com>
2023-03-29 19:43:31 +0200

    Implemented new changes to the network dll, which includes ssl

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ using System.Net.Sockets;
 using System.Threading.Tasks;
 using HTTPParser;
 using System.Threading;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
+using System.Runtime.ConstrainedExecution;
 
 namespace PiServer
 {
@@ -16,8 +19,9 @@ namespace PiServer
         private const int apiPort = 8080;
         private static int port;
         private static ServerTcp server = new ServerTcp(100, 1024 * 4);
+        private static ServerTcpSSL sslServer = new ServerTcpSSL(test(), 100, 1024 * 4);
         private static System.Text.Encoding utf8 = System.Text.Encoding.UTF8;
-        private delegate Task requestHandler(Request request, ReceiveResult receiveResult);
+        private delegate Task requestHandler(Request request, TcpReceiveResult receiveResult);
         private static Dictionary<string, requestHandler> requestHandlers = new Dictionary<string, requestHandler>{
             {"/", SendIndex},
             {"/user_login", UserLogin},
@@ -28,19 +32,28 @@ namespace PiServer
             {"/createaccount", CreateAccountPage},
             {"/newaccount", NewAccount},
         };
-
+        static X509Certificate2 test()
+        {
+            using var cert = X509Certificate2.CreateFromPemFile("fullchain.pem", "privkey.pem");
+            return new X509Certificate2(cert.Export(X509ContentType.Pkcs12));
+        }
         static void Main(string[] args)
         {
             port = 80;
-
-            if(args.Length != 0 && int.TryParse(args[0], out int value))
+            
+            if (args.Length != 0 && int.TryParse(args[0], out int value))
                 port = value;
 
             server.clientAccepted += OnAccept;
             server.onSend += OnSend;
             server.onClientClosed += OnClientClosed;
-            bool success = server.StartListening(port, out string err);
-            if(!success)
+
+            sslServer.clientAccepted += OnAcceptSSL;
+            sslServer.onSend += OnSend;
+            sslServer.onClientClosed += OnClientClosed;
+            bool success = sslServer.StartListening(443, out string er);
+            success = server.StartListening(port, out string err);
+            if (!success)
             {
                 Console.WriteLine("The server failed to start.\n Error: {0}\n Closing application...", err);
                 return;
@@ -61,40 +74,56 @@ namespace PiServer
                     case "stop":
                         goto shutdown;
                     case "clients":
-                        Console.WriteLine("Number of clients: " + server.connectedClients);
+                        Console.WriteLine("Number of clients: " + sslServer.connectedClients);
                         break;
 
                     default:
                         break;
-                }
-                   
+                } 
             }
 
             shutdown:
             server.Shutdown();
+            sslServer.Shutdown();
         }
-
         private static async void OnAccept()
         {
-            if (!server.FetchWaitingClient(out TcpClient client))
+            if (!server.FetchWaitingClient(out ClientTcp client, -1))
                 return;
-           
-            Console.WriteLine("{0} connected", client.Client.RemoteEndPoint);
 
-            ReceiveResult rr;
-           
+            Console.WriteLine("{0} connected", client.client.Client.RemoteEndPoint);
+
+            TcpReceiveResult rr;
+
             do
             {
                 rr = await server.ReceiveAsync(client);
             } while (rr.success && await OnReceive(rr));
+
+
+            server.CloseClientSocket(client, -1);
+        }
+        private static async void OnAcceptSSL()
+        {
+            if (!sslServer.FetchWaitingClient(out ClientTcp client, -1))
+                return;
+           
+            Console.WriteLine("{0} connected", client.client.Client.RemoteEndPoint);
+
+            TcpReceiveResult rr;
+           
+            do
+            {
+                rr = await sslServer.ReceiveAsync(client);
+            } while (rr.success && await OnReceive(rr));
             
             
-            server.CloseClientSocket(client);
+            sslServer.CloseClientSocket(client, -1);
         }
 
         
         //Returns: A bool that indicates if the connection should be kept alive or not
-        private static async Task<bool> OnReceive(ReceiveResult rr)
+        private static async Task<bool> OnReceive(TcpReceiveResult rr)
         {
 
             //size==0 Means that the client sent an empty message which often indicates end of transmission in this case
@@ -103,7 +132,7 @@ namespace PiServer
                 return false;
             }
 
-            Console.WriteLine("ep:{0} size:{1} socket type:{2}", rr.remoteEndPoint, rr.size, rr.socketType);
+            Console.WriteLine("ep:{0} size:{1} socket type:{2}", rr.client.client.Client.RemoteEndPoint, rr.size, rr.socketType);
             string receivedMsg = utf8.GetString(rr.buffer);
             //Console.WriteLine(receivedMsg);
             var req = new Request(receivedMsg);
@@ -128,14 +157,14 @@ namespace PiServer
                         res.SetHeader("Connection", "keep-alive");
                         res.SetHeader("Content-Length", GetFileSize("Assets"+req.element).ToString());
                     }
-                    await server.SendFileAsync("Assets" + req.element, rr.remoteEndPoint, 0, null, utf8.GetBytes(res.GetMsg()));
+                    await sslServer.SendFileAsync("Assets" + req.element, rr.client, 0, null, utf8.GetBytes(res.GetMsg()));
                 }
             }
-            catch (Exception e) when (ExceptionFilter(e, server, rr)) { return false; }
+            catch (Exception e) when (ExceptionFilter(e, sslServer, rr)) { return false; }
             
             return keepAlive;
         }
-        private static bool ExceptionFilter(Exception e, Server server, ReceiveResult rr)
+        private static bool ExceptionFilter(Exception e, ServerTcp server, TcpReceiveResult rr)
         {
             if (e is SocketException)
             {
@@ -146,7 +175,7 @@ namespace PiServer
             else if (e is DirectoryNotFoundException || e is FileNotFoundException || e is UnauthorizedAccessException)
             {
                 byte[] code = System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 404 not found \r\n\r\n");
-                server.Send(code, rr.remoteEndPoint);
+                server.Send(code, rr.client);
                 Console.WriteLine(e.Message);
                 return true;
             }
@@ -154,23 +183,23 @@ namespace PiServer
             return false;
         }
 
-        private static Task SendIndex(Request req, ReceiveResult rr)
+        private static Task SendIndex(Request req, TcpReceiveResult rr)
         {
             Response res = new Response(200);
             res.SetHeader("Connection", "keep-alive");
             res.SetHeader("Content-Length", GetFileSize("Assets/html/index.html").ToString());
-            return server.SendFileAsync("Assets/html/index.html", rr.remoteEndPoint, 0, null, utf8.GetBytes(res.GetMsg()));
+            return sslServer.SendFileAsync("Assets/html/index.html", rr.client, 0, null, utf8.GetBytes(res.GetMsg()));
         }
 
-        private static Task UserLogin(Request req, ReceiveResult rr)
+        private static Task UserLogin(Request req, TcpReceiveResult rr)
         {
             Response res = new Response(200);
             res.SetHeader("Connection", "keep-alive");
             res.SetHeader("Content-Length", GetFileSize("Assets/html/user_login.html").ToString());
-            return server.SendFileAsync("Assets/html/user_login.html", rr.remoteEndPoint, 0, null, utf8.GetBytes(res.GetMsg()));
+            return sslServer.SendFileAsync("Assets/html/user_login.html", rr.client, 0, null, utf8.GetBytes(res.GetMsg()));
         }
 
-        private static Task SendVideo(Request req, ReceiveResult rr)
+        private static Task SendVideo(Request req, TcpReceiveResult rr)
         {
             var res = new Response();
             long offset = 0;
@@ -188,14 +217,14 @@ namespace PiServer
                 res.SetHeader("Content-Length", string.Format("{0}", (end ?? fileLength-1) - offset+1));
             }
 
-            return server.SendFileAsync("Assets/"+req.element, rr.remoteEndPoint, offset, end, utf8.GetBytes(res.GetMsg()));
+            return sslServer.SendFileAsync("Assets/"+req.element, rr.client, offset, end, utf8.GetBytes(res.GetMsg()));
         }
 
-        private static async Task<Task> PcStarter(Request req, ReceiveResult rr)
+        private static async Task<Task> PcStarter(Request req, TcpReceiveResult rr)
         {
             ClientTcp client = new ClientTcp(2048);
             if(!await client.Connect(apiAddress, apiPort))
-                return SendInternalServerError(rr.remoteEndPoint);
+                return SendInternalServerError(rr.client);
             
             var re = new Request();
             re.method = "GET";
@@ -204,21 +233,21 @@ namespace PiServer
             re.SetHeader("Connection", "keep-alive");
            
             var bytes = utf8.GetBytes(re.GetMsg());
-            client.Send(bytes, bytes.Length);
+            client.Send(bytes);
             ReceiveResult apiRR = await client.ReceiveAsync();
             client.Shutdown();
             Console.WriteLine(utf8.GetString(apiRR.buffer));
             if(!apiRR.success)
-                return SendInternalServerError(rr.remoteEndPoint);
+                return SendInternalServerError(rr.client);
             
-            return server.SendAsync(apiRR.buffer, rr.remoteEndPoint);
+            return sslServer.SendAsync(apiRR.buffer, rr.client);
         }
 
-        private static async Task<Task> Login(Request req, ReceiveResult rr)
+        private static async Task<Task> Login(Request req, TcpReceiveResult rr)
         {
             var api = new ClientTcp(2048);
             if(!await api.Connect(apiAddress, apiPort))
-                return SendInternalServerError(rr.remoteEndPoint);
+                return SendInternalServerError(rr.client);
 
             var apiReq = new Request();
             apiReq.element = "/login";
@@ -230,36 +259,37 @@ namespace PiServer
             apiReq.SetHeader("Content-Length", apiReq.body.Length.ToString());
             Console.WriteLine(apiReq.GetMsg());
             var bytes = utf8.GetBytes(apiReq.GetMsg());
-            api.Send(bytes, bytes.Length);
+            api.Send(bytes);
             var apiRR = await api.ReceiveAsync();
             api.Shutdown();
             if(!apiRR.success)
-                return SendInternalServerError(rr.remoteEndPoint);
+                return SendInternalServerError(rr.client);
             var apiRes = new Response(utf8.GetString(apiRR.buffer));
             if(apiRes.code != 200)
-                return server.SendAsync(apiRR.buffer, rr.remoteEndPoint);
+                return sslServer.SendAsync(apiRR.buffer, rr.client);
             
             //var res = new Response(apiRes.GetMsg());
             //res.code = 303;
 
             //res.SetHeader("Location", "/");
             //Console.WriteLine(res.GetMsg());
-            return server.SendAsync(apiRR.buffer, rr.remoteEndPoint);
+            return sslServer.SendAsync(apiRR.buffer, rr.client);
         }
 
-        private static async Task CreateAccountPage(Request req, ReceiveResult rr)
+        private static async Task CreateAccountPage(Request req, TcpReceiveResult rr)
         {
+            string file = "Assets/html/createAccount.html";
             var res = new Response(200);
             res.SetHeader("Content-Type", "text/html");
-        
-            await server.SendFileAsync("Assets/html/createAccount.html", rr.remoteEndPoint, 0, null, utf8.GetBytes(res.GetMsg()));
+            res.SetHeader("Content-Length", GetFileSize(file).ToString());
+            await sslServer.SendFileAsync(file, rr.client, 0, null, utf8.GetBytes(res.GetMsg()));
         }
 
-        private static async Task<Task> NewAccount(Request req, ReceiveResult rr)
+        private static async Task<Task> NewAccount(Request req, TcpReceiveResult rr)
         {
             var api = new ClientTcp(2048);
             if(!await api.Connect(apiAddress, apiPort))
-                return SendInternalServerError(rr.remoteEndPoint);
+                return SendInternalServerError(rr.client);
             
             var apiReq = new Request();
             apiReq.element = req.element;
@@ -271,26 +301,26 @@ namespace PiServer
             apiReq.SetHeader("Content-Length", apiReq.body.Length.ToString());
 
             var bytes = utf8.GetBytes(apiReq.GetMsg());
-            api.Send(bytes, bytes.Length);
+            api.Send(bytes);
             var apiRR = await api.ReceiveAsync();
             if(!apiRR.success)
-                return SendInternalServerError(rr.remoteEndPoint);
+                return SendInternalServerError(rr.client);
             
-            return server.SendAsync(apiRR.buffer, rr.remoteEndPoint);
+            return sslServer.SendAsync(apiRR.buffer, rr.client);
         }
 
-        private static Task SendBadRequest(IPEndPoint remoteEndPoint)
+        private static Task SendBadRequest(ClientTcp client)
         {
-            return server.SendAsync(utf8.GetBytes(new Response(400).GetMsg()), remoteEndPoint);
+            return client.SendAsync(utf8.GetBytes(new Response(400).GetMsg()));
         }
-        private static Task SendInternalServerError(IPEndPoint remoteEndPoint)
+        private static Task SendInternalServerError(ClientTcp client)
         {
-            return server.SendAsync(utf8.GetBytes(new Response(500).GetMsg()), remoteEndPoint);
+            return client.SendAsync(utf8.GetBytes(new Response(500).GetMsg()));
         }
 
         private static void OnClientClosed(IPEndPoint ep)
         {
-            Console.WriteLine("Removed {0}, {1} remaining", ep, server.ConnectedClients());
+            Console.WriteLine("Removed {0}, {1} remaining", ep, sslServer.connectedClients);
         }
 
         
